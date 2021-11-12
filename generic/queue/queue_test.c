3f5016f70cac7d7e1cde7ea103b10e032c4df011
canplane <canplane@gmail.com>
2021-11-13 06:47:15 +0900

    211113-0647

--- generic/queue/queue_test.c
+++ generic/queue/queue_test.c
@@ -1,17 +1,18 @@
-#include        <stdio.h>
+#include		<stdio.h>
 #include		"queue.c"
 
+#define			MAXNUM				10
 
 int main()
 {
-    int _q_container[100 + 1];
+	int _q_container[MAXNUM + 1];
 	Queue q = Queue__init(_q_container);
 
-    for (int i = 0; i < 10; i++) {
-        Queue__push(&q, i);
-    }
-    while (!Queue__empty(&q)) {
-        printf("%d\n", Queue__front(&q, int));
-        Queue__pop(&q);
-    }
+	for (int i = 0; i < MAXNUM; i++) {
+		Queue__push(&q, i);
+	}
+	while (!Queue__empty(&q)) {
+		printf("%d\n", Queue__front(&q, int));
+		Queue__pop(&q);
+	}
 }
