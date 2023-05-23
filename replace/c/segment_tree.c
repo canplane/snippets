116aad9507bbd7d86893b5737feadf88ca4b3368
canplane <canplane@gmail.com>
2023-05-23 20:50:27 +0900

    segment tree added

--- replace/c/segment_tree.c
+++ replace/c/segment_tree.c
@@ -2,37 +2,37 @@
 
 #define		MAX_N		100000
 int seg[1 + 4 * MAX_N], seg_idx[1 + MAX_N];
-int seg_init(int l, int r, int i)
+int seg_init(int l, int r, int i)					// [l, r)
 {
-	if (l == r) {
+	if (l + 1 == r) {
 		seg_idx[l] = i;
 		return seg[i] = 1;
 	}
 
 	int m = (l + r) / 2;
-	return seg[i] = seg_init(l, m, 2 * i) + seg_init(m + 1, r, 2 * i + 1);
+	return seg[i] = seg_init(l, m, 2 * i) + seg_init(m, r, 2 * i + 1);
 }
 int seg_query(int l, int r, int i, int key)			// key -> sum
 {
-	if (l == r)
+	if (l + 1 == r)
 		return seg[i];
 
 	int m = (l + r) / 2;
-	if (key <= m)
+	if (key < m)
 		return seg_query(l, m, 2 * i, key);
 	else
-		return seg[2 * i] + seg_query(m + 1, r, 2 * i + 1, key);
+		return seg[2 * i] + seg_query(m, r, 2 * i + 1, key);
 }
 int seg_inv_query(int l, int r, int i, int sum)		// sum -> key
 {
-	if (l == r)
+	if (l + 1 == r)
 		return l;
 
 	int m = (l + r) / 2;
 	if (sum <= seg[2 * i])
 		return seg_inv_query(l, m, 2 * i, sum);
 	else
-		return seg_inv_query(m + 1, r, 2 * i + 1, sum - seg[2 * i]);
+		return seg_inv_query(m, r, 2 * i + 1, sum - seg[2 * i]);
 }
 
 
@@ -51,14 +51,17 @@ int main()
 	int N, K;
 	scanf("%d %d", &N, &K);
 
-	seg_init(1, N, 1);
+	seg_init(1, N + 1, 1);
+	for (int key = 1; key < N + 1; key++)
+		printf("%d(%d), ", seg_idx[key], seg_query(1, N + 1, 1, key));
+	printf("\n\n");
 	int sum = seg[1], key;
 	
-	key = seg_inv_query(1, N, 1, sum = 1 + ((sum + K) - 1) % seg[1]);
+	key = seg_inv_query(1, N + 1, 1, sum = 1 + ((sum + K) - 1) % seg[1]);
 	printf("<%d", key);
 	seg_update(seg_idx[key]), sum--;
 	while (seg[1] > 0) {
-		key = seg_inv_query(1, N, 1, sum = 1 + ((sum + K) - 1) % seg[1]);
+		key = seg_inv_query(1, N + 1, 1, sum = 1 + ((sum + K) - 1) % seg[1]);
 		seg_update(seg_idx[key]), sum--;
 		printf(", %d", key);
 	}
