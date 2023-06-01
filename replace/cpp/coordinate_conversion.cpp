4aec7a8ff91df81d33e9ce5e0d25c3ea62c47a5d
canplane <canplane@gmail.com>
2023-06-01 20:36:40 +0900

    coordinate conversion added

---
+++ replace/cpp/coordinate_conversion.cpp
@@ -0,0 +1,14 @@
+// 좌표 압축: { 1, 10, 5, 3 } -> { 0, 3, 2, 1 }
+// 최댓값은 (배열 사이즈 - 1) 보장
+template <class T>
+void convert_coord(T arr[], int n)
+{
+	vector<pair<T, int>> v(n);
+	for (int i = 0; i < n; i++) {
+		v[i] = { arr[i], i };
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; i++) {
+		arr[v[i].second] = i;
+	}
+}
