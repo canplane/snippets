0b1da66abe1f8d965bb6273d21eddb0289e1e40f
canplane <canplane@gmail.com>
2023-06-27 19:23:14 +0900

    floor/ceil/round modified

--- replace/py/math/floor.py
+++ replace/py/math/floor.py
@@ -1,9 +1,15 @@
-# C나 Python이나 int()는 truncate: int(3.2) = 3, int(-3.2) = -3
-# 나눗셈: C는 버림처럼 동작 (나머지는 음수 가능), Python은 내림처럼 동작 (나머지는 음수 아님)
-# Python은 math.round 오류! (round half to even)
-#	int(f"{x:.0f}"), floor(x + 0.5)
+# * C나 Python이나 int()는 truncate: int(3.2) = 3, int(-3.2) = -3
+#
+# * 나눗셈:
+#	C는 버림처럼 동작 (나머지는 음수 가능),
+#	Python은 내림처럼 동작 (나머지는 음수 아님)
+#
+# * 라운딩:
+# 	Python은 math.round 오류! (round half to even)
+# 	printf 포맷팅 역시 라운딩 오류가 있으니 쓰지 말자
+#	이게 제일 좋음: floor(x + 0.5)
 
-def _floor(x: float):	# OR int(x // 1)
+def _floor(x: float):	# OR int(x // 1) if Python (not C!)
 	_x = int(x)
 	if x == _x:	return _x
 	return _x - (x < 0)
